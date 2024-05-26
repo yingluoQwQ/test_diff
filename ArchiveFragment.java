35c0a934873b2da6489c90d8b8304078ca1269a5
y1nG1u0 <1301736756@qq.com>
2024-05-26 17:05:03 +0800

    ...

--- ArchiveFragment.java
+++ ArchiveFragment.java
@@ -1,15 +1,12 @@
 package me.sadraa.detoxiom;
 
 
-import android.arch.persistence.room.Room;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 
 /**
@@ -29,19 +23,27 @@ import java.util.ListIterator;
 public class ArchiveFragment extends Fragment {
     TextView authorArchive;
     ArrayList<QuoteDbModel> quoteDbModelList;
-    ArrayList<QuoteDbModel> quoteDbModelListRV;
     QuoteDb quoteDb;
     RecyclerView rv;
     RVAdapter rvAdapter;
-    List<QuoteDbModel> mList;
 
 
     public void test(){
-        mList = quoteDb.quoteDao().getAll();
-        quoteDbModelList = new ArrayList<>();
-        quoteDbModelListRV = new ArrayList<>(mList.size());
-        for (QuoteDbModel q:mList) {
-            quoteDbModelListRV.add(q);
-        }
+        final List<QuoteDbModel> mList = quoteDb.quoteDao().getAll();
+        ArrayList<QuoteDbModel> quoteDbModelListRV = convertListQuoteToArray(mList);
     }
+
+    public ArrayList<QuoteDbModel> convertListQuoteToArray(List<QuoteDbModel> mList){
+        if (mList != null) {
+            quoteDbModelList = new ArrayList<>(mList.size());
+            for (QuoteDbModel q:mList) {
+                quoteDbModelList.add(q);
+            }
+            return quoteDbModelList;
+        }else{
+            return null;
+        }
+
+    }
+
 }
